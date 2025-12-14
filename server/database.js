4e8e23b6f6f13dd399553e7325234d88dbb2db35
Jessidy <jessicamartinssoares@gmail.com>
2025-12-14 15:39:54 +0000

    ConnectionAppV7

--- server/database.js
+++ server/database.js
@@ -1,19 +1,16 @@
 const mysql = require("mysql2");
 
 const connection = mysql.createConnection({
-    host: process.env.DB_HOST || "ia8ezm.h.filess.io",
-    user: process.env.DB_USER || "StepOwl_worryedge",
-    password: process.env.DB_PASSWORD || "8b372de93641d9dbf4d33ec7fe66f7d3249e790c",
-    port: process.env.DB_PORT || 61002,
-    database: process.env.DB_NAME || "StepOwl_worryedge"
+    host:"ia8ezm.h.filess.io",
+    user:"StepOwl_worryedge",
+    password:"8b372de93641d9dbf4d33ec7fe66f7d3249e790c",
+    port:61002,
+    database:"StepOwl_worryedge"
 });
 
 connection.connect(err => {
-    if (err) {
-        console.error("nao consigo a db:", err);
-        return;
-    }
-    console.log("✅ Conectado ao MySQL remoto!");
+    if (err) return console.error("Erro na conexão:", err);
+    console.log("Conectou com sucesso!");
 });
 
 module.exports = connection;
